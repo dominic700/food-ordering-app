17c7b3fbfa40ea3e0683d40950580b529b6a6b3e
dominic700 <udekupasu799@gmail.com>
2026-06-28 16:43:26 -0400

    fix

--- frontend/src/components/BottomNav.jsx
+++ frontend/src/components/BottomNav.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useStore from '../store/useStore.js';
 import telegram from '../telegram.js';
-import useLanguage from '../../hooks/useLanguage.js';
+import useLanguage from '../hooks/useLanguage.js';
 
 
  
